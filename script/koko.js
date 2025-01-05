c2ce218082c9b497ae01a879e83db11ada5a4ccd
awelaw <campros380@gmail.com>
2025-01-05 05:42:20 -0800

    Delete script directory

--- script/koko.js
+++
@@ -1,9 +0,0 @@
-const backlink = document.getElementById('goBack');
-
-backlink.addEventListener('click', function (event){
-    event.preventDefault();
-    window.location.href = 'resp2-login.html';
-});
-document.getElementById("confirm").onclick = function () {
-    document.getElementById("azoz").textContent = 'Azoz ya jrar';
-};
